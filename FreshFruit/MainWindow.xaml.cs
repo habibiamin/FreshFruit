1fa271444194ff86f765bf84248591a096c2acda
habibiamin <61926608+habibiamin@users.noreply.github.com>
2020-11-18 20:24:11 +0700

    Add files via upload

---
+++ FreshFruit/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using FreshFruit.controller;
+using FreshFruit.model;
+using FruitSeller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FreshFruit
+{
+    public partial class MainWindow : Window, BucketEventListener
+    {
+
+        Seller habibi;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Bucket keranjangBuah = new Bucket(2);
+            BucketController bucketController = new BucketController(keranjangBuah, this);
+
+            habibi = new Seller("habibi", bucketController);
+
+            ListBoxBucket.ItemsSource = keranjangBuah.findAll();
+        }
+
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit anggur = new Fruit("Anggur");
+            habibi.addFruit(anggur);
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit Apel = new Fruit("Apel");
+            habibi.addFruit(Apel);
+
+        }
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit Pisang = new Fruit("Pisang");
+            habibi.addFruit(Pisang);
+
+        }
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Fruit jeruk = new Fruit("Jeruk");
+            habibi.addFruit(jeruk);
+
+        }
+
+        public void onFailed(string message)
+        {
+            MessageBox.Show(message, "Warning");
+        }
+
+        public void onSucceed(string message)
+        {
+            ListBoxBucket.Items.Refresh();
+        }
+    }
+}
